926e96db994cb96eebefa7c37bb25d75763d8199
pierfier <fabrispierre@gmail.com>
2017-06-05 19:55:33 -0400

    Added a point to work on

--- src/Downloader.cpp
+++ src/Downloader.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+Downloader::Downloader(const string& key, const string & id, 
+                        const string & host) : 
+                        key_(key), id_(id), host_(host) {
+    
+    request_ = new HTTPRequest(key, id, host);
+}
+
 Downloader::~Downloader(){
     delete request_;
 }
@@ -12,13 +19,10 @@ void Downloader::getImages(){
     string linksXML;
     request_->requestLinks(linksXML);
     
+    //TODO here is where the thread pool should be called
+
     //DEBUG
     //cout << linksXML << endl;
 
     request_->getImages(linksXML);
-}
-
-Downloader::Downloader(const string& key, const string & id, const string & host) : 
-                                           key_(key), id_(id), host_(host) {
-    request_ = new HTTPRequest(key, id, host);
 }
