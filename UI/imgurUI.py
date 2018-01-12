abbac11a428a1a56f2af4af833ec4a50d0f61654
pierfier <fabrispierre@gmail.com>
2018-01-12 15:19:30 -0500

    Working on the html and web server data passage

--- UI/imgurUI.py
+++ UI/imgurUI.py
@@ -1,10 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
-# Show the main page
+# Show main.html
 @app.route("/")
 def index():
     render_template()
+
+@app.route('/handle_data', method=['POST'])
+# Receive the request data
+def handleRequestData():
+    album_id = request.form['album_id']
+    title = request.form['title']
+    author = request.form['author']
+    use_first_image = request.form['use_first_image']
+
+    
+# Used to call the imgurfetch executable to download all of the images to the
+# server
+# TODO need to implement
+def downloadAlbum():
+    print("Not Implemented yet")
 
 if __name__ == "__main__":
     app.run()
--- UI/readme.md
+++ UI/readme.md
@@ -1,3 +1,15 @@
 Directory for the frontend development
 
 The web framework used will be Flask in Python
+
+This is the main outline of the website:
+
+Form:
+
+Get Album ID
+
+Title
+
+Author
+
+<checkbox> First image is cover page
